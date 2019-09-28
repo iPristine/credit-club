fbd3a5a2183e64dd61b0e114458ad4f39c7ed957
Малых Денис <iPristine@Mac-mini-Vladimir.local>
2019-09-28 18:57:56 +0500

    add db

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from "history";
 
 import "./index.sass";
 import App from "./components/app/index";
